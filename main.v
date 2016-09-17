4be257d0384fe4d67a6eadd530118615353ff175
Juan Enrique Segebre <juansegebre@gmail.com>
2016-09-17 16:57:20 +0000

    main.v edited online with Bitbucket, por problemas de git

--- main.v
+++ main.v
@@ -1,4 +1,4 @@
-module VGA_main(
+module main(
     input clk50mhz,
     input reset,
     output [2:0] rgb,
@@ -10,6 +10,8 @@ module VGA_main(
 	
 	wire[9:0] plataform_start;
 	wire[9:0] plataform_end;
+	wire[9:0] hole_start;
+	wire[9:0] hole_end;
 	
 	// synthesis attribute CLKFX_DIVIDE of vga_clock_dcm is 4
 	// synthesis attribute CLKFX_MULTIPLY of vga_clock_dcm is 2
@@ -19,12 +21,15 @@ module VGA_main(
 		vga_clk = !vga_clk;
 		
 	get_plataform_y get_pos( vga_clk, plataform_start, plataform_end);
+	get_plataform_hole get_hole( vga_clk, hole_start, hole_end);
 
-	VGA_Logic vga_logic (
+	vga_logic vga (
 		.clk(vga_clk),
 		.reset(reset),
 		.plataform_start(plataform_start),
 		.plataform_end(plataform_end),
+		.hole_start(hole_start),
+		.hole_end(hole_end),
 		.hsync(hsync),
 		.vsync(vsync),
 		.rgb(rgb)
